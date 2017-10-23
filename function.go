ebd5bc80050af1c62d15a3b59cc0ff91be402df7
behouba <behouba@gmail.com>
2017-10-24 00:02:38 +0300

    function with multiple return value

---
+++ function.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func location(name, city string) (region, continent string) {
+	switch city {
+	case "New York", "LA", "Chicago", "Manassé":
+		continent = "North America"
+	default:
+		continent = "Unknow"
+	}
+	return
+}
+func main() {
+	region, continent := location("Matt", "LA")
+	fmt.Printf("%s lives in %s", region, continent)
+}
--- main.go
+++ main.go
@@ -2,13 +2,23 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
-func calCircleArea(radius float32) float32 {
-	return radius * radius * math.Pi
+func location(city string) (string, string) {
+	var region string
+	var continent string
+
+	switch city {
+	case "Los Angeles", "LA", "Santa Monica":
+		region, continent = "California", "North America"
+	case "New York", "NYC":
+		region, continent = "New York", "North America"
+	default:
+		region, continent = "Unknow", "Unknow"
+	}
+	return region, continent
 }
 
 func main() {
-	fmt.Println(calCircleArea(50))
+	fmt.Println(location("New York"))
 }
